7337c21c14b2ff4252d47388ae36799046a8eb99
jdmarmen <jdmarmen@etsinf.upv.es>
2024-10-15 19:25:01 +0200

    Added list headers

---
+++ List.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template <typename T> 
+class List 
+{
+    public:
+        // ... aquí los métodos virtuales puros
+        virtual void insert(int pos, T e)=0;
+        virtual void append(T e)=0;
+        virtual void prepend(T e)=0;
+        virtual T remove(int pos)=0;
+        virtual T get(int pos)=0;
+        virtual int search(T e)=0,
+        virtual bool empty()=0;
+        virtual int size()=0;
+
+
+
+
+};
