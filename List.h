0190448e8b73754b64733ed7101f0743789f9867
jdmarmen <jdmarmen@etsinf.upv.es>
2024-10-21 00:54:45 +0200

    changed a , for a ; in line 13

--- List.h
+++ List.h
@@ -10,7 +10,7 @@ class List
         virtual void prepend(T e)=0;
         virtual T remove(int pos)=0;
         virtual T get(int pos)=0;
-        virtual int search(T e)=0,
+        virtual int search(T e)=0;
         virtual bool empty()=0;
         virtual int size()=0;
 
